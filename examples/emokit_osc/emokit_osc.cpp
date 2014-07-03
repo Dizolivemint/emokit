79696f64b6d5062c437d6eb2cf708a672aa01b12
Kyle Machulis <kyle@nonpolynomial.com>
2014-07-02 18:46:50 -0700

    Updated emokit OSC example so it at least compiles

--- examples/emokit_osc/emokit_osc.cpp
+++ examples/emokit_osc/emokit_osc.cpp
@@ -1,5 +1,5 @@
-/* 
-    Simple example of sending an OSC message using oscpack.
+/*
+   Simple example of sending an OSC message using oscpack.
 */
 
 #include <cstdio>
@@ -9,7 +9,7 @@
 #include <iostream>
 #include "oscpack/osc/OscOutboundPacketStream.h"
 #include "oscpack/ip/UdpSocket.h"
-#include "libepoc.h"
+#include "emokit/emokit.h"
 
 #define ADDRESS "127.0.0.1"
 #define PORT 9997
@@ -29,65 +29,44 @@ int main(int argc, char* argv[])
 	signal(SIGQUIT, sigproc);
 #endif
 
-    UdpTransmitSocket transmitSocket( IpEndpointName( ADDRESS, PORT ) );
-    
-    char buffer[OUTPUT_BUFFER_SIZE];
+  UdpTransmitSocket transmitSocket( IpEndpointName( ADDRESS, PORT ) );
 
+  char buffer[OUTPUT_BUFFER_SIZE];
 
 	FILE *input;
 	FILE *output;
-	enum headset_type type;
-  
-	char raw_frame[32];
-	struct epoc_frame frame;
-	epoc_device* d;
-	uint8_t data[32];
-	if (argc < 2)
-	{
-		fputs("Missing argument\nExpected: epocd [consumer|research|special]\n", stderr);
-		return 1;
-	}
-  
-	if(strcmp(argv[1], "research") == 0)
-		type = RESEARCH_HEADSET;
-	else if(strcmp(argv[1], "consumer") == 0)
-		type = CONSUMER_HEADSET;
-	else if(strcmp(argv[1], "special") == 0)
-		type = SPECIAL_HEADSET;
-	else {
-		fputs("Bad headset type argument\nExpected: epocd [consumer|research|special] source [dest]\n", stderr);
-		return 1;
-	}
-  
-	epoc_init(type);
 
-	d = epoc_create();
-	printf("Current epoc devices connected: %d\n", epoc_get_count(d, EPOC_VID, EPOC_PID));
-	if(epoc_open(d, EPOC_VID, EPOC_PID, 0) != 0)
+	char raw_frame[32];
+	struct emokit_frame frame;
+	emokit_device* d;
+	uint8_t data[32];
+
+	d = emokit_create();
+	printf("Current epoc devices connected: %d\n", emokit_get_count(d, EMOKIT_VID, EMOKIT_PID));
+	if(emokit_open(d, EMOKIT_VID, EMOKIT_PID, 0) != 0)
 	{
 		printf("CANNOT CONNECT\n");
 		return 1;
 	}
 	while(1)
 	{
-		if(epoc_read_data(d, data) > 0)
+		if(emokit_read_data(d) > 0)
 		{
-			epoc_get_next_frame(&frame, data);
+			frame = emokit_get_next_frame(d);
 			osc::OutboundPacketStream p( buffer, OUTPUT_BUFFER_SIZE );
 			p << osc::BeginBundleImmediate
 			  << osc::BeginMessage( "/epoc/channels" )
 			  << frame.F3 << frame.FC6 << frame.P7 << frame.T8 << frame.F7 << frame.F8 << frame.T7 << frame.P8 << frame.AF4 << frame.F4 << frame.AF3 << frame.O2 << frame.O1 << frame.FC5 << osc::EndMessage
-			  << osc::BeginMessage( "/epoc/gyro" ) 
+			  << osc::BeginMessage( "/epoc/gyro" )
 			  << frame.gyroX << frame.gyroY << osc::EndMessage
 			  << osc::EndBundle;
-    
+
 			transmitSocket.Send( p.Data(), p.Size() );
 		}
 	}
 
-	epoc_close(d);
-	epoc_delete(d);
+	emokit_close(d);
+	emokit_delete(d);
 	return 0;
 
 }
-
