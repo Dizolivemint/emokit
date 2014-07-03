d5f602dc4ce39ef363d194bd04e1a33b7cb44cc6
Kyle Machulis <kyle@nonpolynomial.com>
2014-07-02 18:54:27 -0700

    Bringing in vincentdm05's version of emokit_osc from https://github.com/sinlab-semester-2012/EmoCopter/blob/master/server/examples/emokit_osc/emokit_osc.cpp

--- examples/emokit_osc/emokit_osc.cpp
+++ examples/emokit_osc/emokit_osc.cpp
@@ -1,5 +1,5 @@
-/*
-   Simple example of sending an OSC message using oscpack.
+/* 
+    Simple *working* example of sending an OSC message using oscpack.
 */
 
 #include <cstdio>
@@ -12,13 +12,15 @@
 #include "emokit/emokit.h"
 
 #define ADDRESS "127.0.0.1"
-#define PORT 9997
+#define PORT 7000
 
 #define OUTPUT_BUFFER_SIZE 4096
 
+#define FREQ 1000000/128
+
 void sigproc(int i)
 {
-	std::cout << "closing epoc and quitting" << std::endl;
+	std::cout << "\nClosing emokit and quitting." << std::endl;
 	exit(0);
 }
 
@@ -29,39 +31,82 @@ int main(int argc, char* argv[])
 	signal(SIGQUIT, sigproc);
 #endif
 
-  UdpTransmitSocket transmitSocket( IpEndpointName( ADDRESS, PORT ) );
+	bool noHelmet = false;
+	if((argc > 1) && (argv[1] == std::string("-n"))) noHelmet = true;
 
-  char buffer[OUTPUT_BUFFER_SIZE];
-
-	FILE *input;
-	FILE *output;
-
-	char raw_frame[32];
-	struct emokit_frame frame;
+    UdpTransmitSocket transmitSocket( IpEndpointName( ADDRESS, PORT ) );
+    
+    char buffer[OUTPUT_BUFFER_SIZE];
+	
 	emokit_device* d;
-	uint8_t data[32];
-
+	
 	d = emokit_create();
 	printf("Current epoc devices connected: %d\n", emokit_get_count(d, EMOKIT_VID, EMOKIT_PID));
-	if(emokit_open(d, EMOKIT_VID, EMOKIT_PID, 0) != 0)
+	if(emokit_open(d, EMOKIT_VID, EMOKIT_PID, 0) != 0 && !noHelmet)
 	{
 		printf("CANNOT CONNECT\n");
 		return 1;
+	} else if(noHelmet) {
+		std::cout << "Sending random data" << std::endl;
 	}
-	while(1)
-	{
-		if(emokit_read_data(d) > 0)
+	
+	if (!noHelmet) {
+		while(true)
 		{
-			frame = emokit_get_next_frame(d);
-			osc::OutboundPacketStream p( buffer, OUTPUT_BUFFER_SIZE );
-			p << osc::BeginBundleImmediate
-			  << osc::BeginMessage( "/epoc/channels" )
-			  << frame.F3 << frame.FC6 << frame.P7 << frame.T8 << frame.F7 << frame.F8 << frame.T7 << frame.P8 << frame.AF4 << frame.F4 << frame.AF3 << frame.O2 << frame.O1 << frame.FC5 << osc::EndMessage
-			  << osc::BeginMessage( "/epoc/gyro" )
-			  << frame.gyroX << frame.gyroY << osc::EndMessage
-			  << osc::EndBundle;
-
-			transmitSocket.Send( p.Data(), p.Size() );
+			if(emokit_read_data(d) > 0)
+			{
+				emokit_get_next_frame(d);
+				struct emokit_frame frame = d->current_frame;
+				
+				std::cout << "\r\33[2K" << "gyroX: " << (int)frame.gyroX
+					<< "; gyroY: " << (int)frame.gyroY
+					<< "; F3: " << frame.F3
+					<< "; FC6: " << frame.FC6
+					<< "; battery: " << (int)d->battery << "%";
+				
+				flush(std::cout);
+				
+				osc::OutboundPacketStream channels( buffer, OUTPUT_BUFFER_SIZE );
+				osc::OutboundPacketStream gyro( buffer, OUTPUT_BUFFER_SIZE );
+				osc::OutboundPacketStream info( buffer, OUTPUT_BUFFER_SIZE );
+				
+				channels << osc::BeginMessage( "/emokit/channels" )
+					<< frame.F3 << frame.FC6 << frame.P7 << frame.T8 << frame.F7 << frame.F8 << frame.T7 << frame.P8 << frame.AF4 << frame.F4 << frame.AF3 << frame.O2 << frame.O1 << frame.FC5 << osc::EndMessage;
+				transmitSocket.Send( channels.Data(), channels.Size() );
+				
+				gyro << osc::BeginMessage( "/emokit/gyro" ) 
+					<< (int)frame.gyroX << (int)frame.gyroY << osc::EndMessage;
+				transmitSocket.Send( gyro.Data(), gyro.Size() );
+				
+				info << osc::BeginMessage( "/emokit/info" )
+					<< (int)d->battery;
+					for (int i = 0; i<14 ; i++) info << (int)d->contact_quality[i];
+					info << osc::EndMessage;
+				transmitSocket.Send( info.Data(), info.Size() );
+			}
+		}
+	} else {
+		while (true) {
+			usleep(FREQ);
+			
+			osc::OutboundPacketStream channels( buffer, OUTPUT_BUFFER_SIZE );
+			osc::OutboundPacketStream gyro( buffer, OUTPUT_BUFFER_SIZE );
+			osc::OutboundPacketStream info( buffer, OUTPUT_BUFFER_SIZE );
+			
+			channels << osc::BeginMessage( "/emokit/channels" );
+			for (int i=0 ; i < 14 ; i++) channels << rand() % 10000;
+			channels << osc::EndMessage;
+			transmitSocket.Send( channels.Data(), channels.Size() );
+			
+			gyro << osc::BeginMessage( "/emokit/gyro" ) 
+				<< rand() % 100 << rand() % 100 << osc::EndMessage;
+			transmitSocket.Send( gyro.Data(), gyro.Size() );
+			
+			info << osc::BeginMessage( "/emokit/info" )
+				<< rand() % 100;
+				for (int i = 0; i<14 ; i++) info << rand() % 50;
+				info << osc::EndMessage;
+			transmitSocket.Send( info.Data(), info.Size() );
 		}
 	}
 
